d1255d5d4a1f771c8081cffa921f85c9bdf2ef2b
abayomi igwubor <igwubor@outlook.com>
2020-05-08 23:38:42 +0100

    added Method Syntax

--- ArticlesAndCategories/Program.cs
+++ ArticlesAndCategories/Program.cs
@@ -11,7 +11,7 @@ namespace ArticlesAndCategories
             List<Article> Articles = new List<Article>();
             Articles.Add(new Article( 1,1,"Jovic returns to Real Madrid!","Real Madrid striker Luka Jovic has returned from the coronavirus lockdown with a foot fracture, the club has revealed."));
             Articles.Add(new Article(2, 1, "LaLiga cool talks over June 20 resumption", "LaLiga insist that there remains no set date for the 2019-20 season to resume. On Thursday, Leganes coach Javier Aguirre claimed that the campaign - which was halted in March - would return on June 20 and be wrapped up five weeks later by July 26."));
-            Articles.Add(new Article(3, 2, "Tinubu: 'My aide died of coronavirus'", "Former Lagos Governor and APC chieftain, Asiwaju Bola Ahmed Tinubu, has disclosed that his late Chief Security Officer, Alhaji Lateef Raheem, who died on Friday, April 25, 2020, passed on from COVID-19 complications."));
+            Articles.Add(new Article(3, 2, "Tinubu: My aide died of coronavirus", "Former Lagos Governor and APC chieftain, Asiwaju Bola Ahmed Tinubu, has disclosed that his late Chief Security Officer, Alhaji Lateef Raheem, who died on Friday, April 25, 2020, passed on from COVID-19 complications."));
             Articles.Add(new Article(4, 2, "Appeal Court affirms Ifeanyi Ubah as senator-elect", "Justice Bello Kawu had on Jan. 17, nullified Ubah's election on grounds that he allegedly used a forged National Examination Council, (NECO) certificate to contest the senatorial election held in Anambra South on Feb. 23, 2019."));
 
 
